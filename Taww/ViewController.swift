d71e32012c81232442063183964686c483df2a41
youngju <asdadssad@naver.coim>
2018-03-26 15:43:50 +0900

    succes

--- Taww/ViewController.swift
+++ Taww/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    private var guessNumber = 0
+    private var countGuesses = 0
+    private var guessAgain = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        guessNumber = Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func guessbutton(_ sender: Any) {
+        if guessAgain{
+            countGuesses = 0
+            input.text!=""
+            guessNumber = Int(arc4random_uniform(100))
+            guessAgain = false
+        }
+        if Int(input.text!) != nil {
+            
+            let inputnum = Int(input.text!)
+            countGuesses += 1
+            
+            if inputnum == guessNumber {
+                resultLabel.text = "Congratulations. You got the number. It took You \(countGuesses) guesses to guess the number Try Again?"
+                guessAgain = true
+                
+                
+            }else if inputnum! < guessNumber {
+                resultLabel.text = "Try higher number !!"
+                
+            }else if inputnum! > guessNumber {
+                resultLabel.text = "Try lower number !!"
+                
+            }
+            input.text = ""
+        }
+        else {
+            resultLabel.text = "Please Enter a Number you are guessing !!"
+        }
+            
+        }
 }
+    
+        
+        
+        
+        
+        
+    
+    
+
 
